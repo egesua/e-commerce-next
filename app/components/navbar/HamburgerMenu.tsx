d28caf647a81a62227463d2a729e2b3ff6a299ac
egesua <egesu06@gmail.com>
2024-05-09 14:08:24 +0300

    hamburger icon added also for mobile res relativeness added.

--- app/components/navbar/HamburgerMenu.tsx
+++ app/components/navbar/HamburgerMenu.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
+import { GiHamburgerMenu } from "react-icons/gi";
 const HamburgerMenu = () => {
   return (
-    <div>
-      HamburgerMenu
+    <div className="relative flex md:hidden">
+      <GiHamburgerMenu size="25" />
     </div>
   )
 }
--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 const Logo = () => {
   return (
     //TODO
