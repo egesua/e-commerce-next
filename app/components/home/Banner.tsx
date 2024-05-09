06c2f00a42eb6557c44d6eb40b093c946ef85ba7
egesua <egesu06@gmail.com>
2024-05-09 14:36:59 +0300

    banner photo upload also heading component created.

--- app/components/home/Banner.tsx
+++ app/components/home/Banner.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image";
 
 const Banner = () => {
   return (
-    <div>
-      Banner
+    <div className="h-[167px] bg-black flex items-center justify-center">
+      <div className="h-[137px] relative w-full">
+        <Image src={"/banner.jpg"} fill alt="bannerPhoto" />
+      </div>
     </div>
   )
 }
